21bf0710913da20061f96ab515e8e0dd844975df
usuario <usuario@roberto>
2017-06-21 16:58:25 -0300

    cambio de las 21/06/2017 14.43

---
+++ TPfinal/struct.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   struct.h
+ * Author: usuario
+ *
+ * Created on 21 de junio de 2017, 16:57
+ */
+
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STRUCT_H */
+
